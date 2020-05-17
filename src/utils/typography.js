b213137397d22d615e097904e71472bfdf39437a
Jason Miller <jason@redconfetti.com>
2020-05-17 11:14:32 -0700

    Replaced starter site with starter blog code

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,8 +1,23 @@
 import Typography from "typography"
-import fairyGateTheme from "typography-theme-fairy-gates"
-// import North from 'typography-theme-north';
+import Wordpress2016 from "typography-theme-wordpress-2016"
 
-const typography = new Typography(fairyGateTheme)
+Wordpress2016.overrideThemeStyles = () => {
+  return {
+    "a.gatsby-resp-image-link": {
+      boxShadow: `none`,
+    },
+  }
+}
 
-export const { scale, rhythm, options } = typography
+delete Wordpress2016.googleFonts
+
+const typography = new Typography(Wordpress2016)
+
+// Hot reload typography in development.
+if (process.env.NODE_ENV !== `production`) {
+  typography.injectStyles()
+}
+
 export default typography
+export const rhythm = typography.rhythm
+export const scale = typography.scale
