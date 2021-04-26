33f347e8c67d80f65a5890c645e524ff100259a3
Jason Miller <jason@redconfetti.com>
2021-04-26 05:41:29 +0000

    Adds build server - Sinatra script

---
+++ build-server/main.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+
+require 'sinatra'
+require 'json'
+
+set :port, 5000
+
+post '/' do
+
+  request.body.rewind
+  payload_body = request.body.read
+  verify_signature(payload_body)
+  push = JSON.parse(params[:payload])
+  "I got some JSON: #{push.inspect}"
+end
+
+def verify_signature(payload_body)
+  signature = 'sha256=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha256'), ENV['SECRET_TOKEN'], payload_body)
+  return halt 500, "Signatures didn't match!" unless Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE_256'])
+end
+
+
